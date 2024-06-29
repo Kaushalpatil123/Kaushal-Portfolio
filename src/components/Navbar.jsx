6f7ee018110562f388b631d5459caa4669414248
kaushal patil <91415972+Kaushalpatil123@users.noreply.github.com>
2024-06-29 16:41:25 +0530

    color added

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,10 +2,12 @@ import React from "react";
 
 const Navbar = () => {
   return (
-    <div className="h-20 shadow-md p-5 flex justify-between">
+    <div className="h-20 p-5 flex justify-between">
       <h1 className="text-3xl font-semibold font-sans">Kaushal Patil ...</h1>
       <div className="flex gap-12 items-center ">
-        <a href="" className="text-xl font-sans hover:scale-75">All Resources</a>
+        <a href="" className="text-xl font-sans hover:scale-75">
+          All Resources
+        </a>
         <button className="h-12 px-4 rounded-xl bg-black text-white p-1 shadow-md font-sans font-semibold">
           Contact Me
         </button>
--- src/components/Spotlight.jsx
+++ src/components/Spotlight.jsx
@@ -6,36 +6,36 @@ import photo from "../assets/giftphoto.gif"
 const Spotlight = () => {
   return (
     <div>
-      <div className="bg-[#252525] w-full py-10 flex">
-        <div className="img w-[50%] justify-center align-middle items-center flex">
-          <div class="fancy-border-radius m-auto overflow-hidden">
-            <img src={photo} alt="img fill" />
+      <div className="bg-[#252525] w-full py-10 ">
+        <div className="flex gap-5 m-auto w-[80%]">
+          <div className="img m-auto">
+            <div class="fancy-border-radius m-auto overflow-hidden">
+              <img src={photo} alt="img fill" />
+            </div>
           </div>
-        </div>
 
-        <div className="intro w-[50%] pt-20 pl-10 pb-10 text-white">
-          <h1 className="text-6xl font-bold mb-5 ">
-            <Typewriter
-              options={{
-                autoStart: true,
-                loop: true,
-                delay: 50,
-                strings: [" Hello, I'm Kaushal Patil..."],
-              }}
-            />
-          </h1>
-          <h1 className="text-4xl font-bold mb-5">
-            A Front-end Developer.
-          </h1>
-          <div className="w-[70%]">
-            <p className="text-[25px]">
-              Hi, I am a coding and frontend development enthusiast, always open
-              to learning new skills and looking for work opportunities. I'm
-              proficient in web development and frontend development, with
-              excellent skills in HTML, CSS, and JavaScript. I also have
-              experience with libraries like React.js. I am very interested in
-              working with new people and learning new skills.
-            </p>
+          <div className="intro pt-20 pl-10 pb-10 text-white m-auto">
+            <h1 className="text-6xl font-bold mb-5 text-gradient">
+              <Typewriter
+                options={{
+                  autoStart: true,
+                  loop: true,
+                  delay: 50,
+                  strings: [" Hello, I'm Kaushal Patil..."],
+                }}
+              />
+            </h1>
+            <h1 className="text-4xl font-bold mb-5">A Front-end Developer.</h1>
+            <div className="w-full">
+              <p className="text-[25px]">
+                Hi, I am a coding and frontend development enthusiast, always
+                open to learning new skills and looking for work opportunities.
+                I'm proficient in web development and frontend development, with
+                excellent skills in HTML, CSS, and JavaScript. I also have
+                experience with libraries like React.js. I am very interested in
+                working with new people and learning new skills.
+              </p>
+            </div>
           </div>
         </div>
       </div>
